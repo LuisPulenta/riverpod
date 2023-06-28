6f63060a6b5eb964ec2e1b8f15907a4af01e7418
LuisPulenta <luisalbertonu@gmail.com>
2023-06-28 19:05:02 -0300

    Cambios

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -3,3 +3,5 @@ export 'state_providers.dart';
 export 'todo_providers.dart';
 export 'future_providers.dart';
 export 'stream_providers.dart';
+export 'todos_state_notifier_provider.dart';
+export 'todos_change_notifier_provider.dart';
--- lib/presentation/screens/09_change_notifier/change_notifier_screen.dart
+++ lib/presentation/screens/09_change_notifier/change_notifier_screen.dart
@@ -1,15 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/providers.dart';
 
-
-class ChangeNotifierScreen extends StatelessWidget {
+class ChangeNotifierScreen extends ConsumerWidget {
   const ChangeNotifierScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Change Notifier Provider'),
       ),
+      body: const _TodoView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          ref.read(todosChangeNotifierProvider.notifier).addTodo();
+        },
+      ),
+    );
+  }
+}
+
+//------------------------- _TodoView -----------------------------
+
+class _TodoView extends ConsumerWidget {
+  const _TodoView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todosChangeNotifier = ref.watch(todosChangeNotifierProvider);
+
+    return Column(
+      children: [
+        // const ListTile(
+        //   title: Text('Listado de invitados'),
+        //   subtitle: Text('Estas son las personas a invitar a la fiesta'),
+        // ),
+
+        // SegmentedButton(
+        //   segments: const [
+        //     ButtonSegment(value: TodoFilter.all, icon: Text('Todos')),
+        //     ButtonSegment(value: TodoFilter.completed, icon: Text('Invitados')),
+        //     ButtonSegment(
+        //         value: TodoFilter.pending, icon: Text('No invitados')),
+        //   ],
+        //   selected: <TodoFilter>{currentFilter},
+        //   onSelectionChanged: (value) {
+        //     ref.read(todoFilterProvider.notifier).state = value.first;
+        //   },
+        // ),
+        const SizedBox(height: 5),
+
+        // Listado de personas a invitar
+        Expanded(
+          child: ListView.builder(
+            itemCount: todosChangeNotifier.todos.length,
+            itemBuilder: (context, index) {
+              final todo = todosChangeNotifier.todos[index];
+              return SwitchListTile(
+                  title: Text(todo.description),
+                  value: todo.done,
+                  onChanged: (value) {
+                    ref
+                        .read(todosChangeNotifierProvider.notifier)
+                        .toggleTodo(todo.id);
+                  });
+            },
+          ),
+        )
+      ],
     );
   }
 }
